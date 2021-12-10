a0c4bb566a2f016c43ee918c0c8c3d00bd96e35a
jennahakulinen <jenna.hakulinen@metropolia.fi>
2021-12-10 20:39:08 +0200

    Nollattu

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
 'use strict';
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const catRoute = require('./routes/catRoute');
-const recipeRoute = require('./routes/recipeRoute')
-const userRoute = require('./routes/userRoute');
-const authRoute = require('./routes/authRoute');
-const passport = require('./utils/pass');
-const { httpError } = require('./utils/errors');
-
 const app = express();
 const port = 3000;
-// const port = 'http://10.114.34.22/'; //???
-app.use(cors());
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.use(express.static('./uploads/'));
-app.use('/thumbnails', express.static('thumbnails'));
-
-app.use(passport.initialize());
-
-app.use('/auth', authRoute);
-app.use('/recipe', passport.authenticate('jwt', {session: false}), recipeRoute);
-app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
-
-app.use((req, res, next) => {
-    const err = httpError('Not found', 404);
-    next(err);
-});
-
-app.use((err, req, res, next) => {
-    res
-    .status(err.status || 500)
-    .json(
-        {
-            error: {
-                message: err.message || 'internal server error', }
-        }
-    );
+app.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
